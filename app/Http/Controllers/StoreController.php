c6c59cfbd1a410524792ac5aeca3ab632898a7e0
Mohammed Omar Abdulkhaleq Esmail <mohamedalaghbari20@gmail.com>
2025-09-04 11:07:23 +0300

    feat: add products viwe

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    //
+
+    public function index(){
+        return view('shop.index');
+    }
+
+    public function products()
+    {
+        return view('shop.products');
+    }
+
+    // تفاصيل منتج واحد
+    public function productDetails($id = null)
+    {
+        // لاحقاً ممكن تمرر بيانات المنتج المحدد
+        return view('shop.product-details');
+    }
+
+    // صفحة السلة
+    public function cart()
+    {
+        return view('shop.cart');
+    }
+
+    // صفحة من نحن
+    public function aboutUs()
+    {
+        return view('shop.about-us');
+    }
+
+    // صفحة تواصل معنا
+    public function contact()
+    {
+        return view('shop.contact');
+    }
 }
+
+
