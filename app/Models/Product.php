cb5c188f69e9e62b57f7c5cdbdb0094c9dd1fd4e
Mohammed Omar Abdulkhaleq Esmail <mohamedalaghbari20@gmail.com>
2025-09-05 12:42:14 +0300

    feat: Implement Create (add new product) functionality

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Product extends Model
 {
     //
+    use HasFactory;
+    public $timestamps = false;
+    protected $fillable = [
+        'name', 
+        'description', 
+        'price', 
+        'on_sale',
+        'image_path'
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'on_sale' => 'boolean'
+    ];
 }
