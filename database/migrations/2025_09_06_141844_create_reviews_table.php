83354254ce3eec12a88174b4302eaf121b5ce5a5
Mohammed Omar Abdulkhaleq Esmail <mohamedalaghbari20@gmail.com>
2025-09-06 17:47:59 +0300

    feat: Add reviews table with factories and seeders and model

---
+++ database/migrations/2025_09_06_141844_create_reviews_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+        $table->id();
+        $table->integer('rating'); 
+        $table->text('comment')->nullable(); 
+            
+            // Foreign key to products table
+        $table->foreignId('product_id')
+                ->constrained()
+                ->onDelete('cascade'); 
+            
+            // Foreign key to users table
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('cascade'); 
+            
+            $table->timestamps();
+            
+            $table->unique(['product_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
