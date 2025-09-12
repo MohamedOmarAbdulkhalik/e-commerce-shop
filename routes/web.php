a66a8993d8f089bd8e996616d76358fc0f21084f
Mohammed Omar Abdulkhaleq Esmail <mohamedalaghbari20@gmail.com>
2025-09-12 18:28:44 +0300

    fix: get the products edit and distroy routs out of the admin group

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,21 @@ Route::middleware(['auth', 'verified'])->group(function () {
     // المنتجات
     Route::get('/products', [ProductController::class, 'index'])->name('products.index');
     Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
+    Route::delete('/delete/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
 
 
     // Admin routes
     Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
-        Route::get('/products', [AdminController::class, 'products'])->name('products');
         Route::get('/categories', [AdminController::class, 'categories'])->name('categories');
         Route::get('/add-product', [ProductController::class, 'create'])->name('products.create');
         Route::post('/store', [ProductController::class, 'store'])->name('products.store');
-        Route::get('/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
         Route::put('/update/{id}', [ProductController::class, 'update'])->name('products.update');
-        Route::delete('/delete/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
     });
 
     // المتجر
+
     Route::get('/cart', [StoreController::class, 'cart'])->name('cart'); 
     Route::get('/aboutUs', [StoreController::class, 'aboutUs'])->name('aboutUs'); 
     Route::get('/contact', [StoreController::class, 'contact'])->name('contact'); 
