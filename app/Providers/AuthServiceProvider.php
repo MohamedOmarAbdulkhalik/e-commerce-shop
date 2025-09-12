8b366fc9f23dc53d6be8bc13d4603bbf3237f6da
Mohammed Omar Abdulkhaleq Esmail <mohamedalaghbari20@gmail.com>
2025-09-12 19:10:40 +0300

    feat: Define 'access-admin-panel' gate

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+    $this->registerPolicies();
+
+    Gate::define('access-admin-panel', function ($user) {
+        if ($user->is_admin) {
+            return true;
+        }
+
+        // تسجيل محاولات غير مصرح بها
+        Log::warning("Unauthorized access attempt by user: {$user->id}");
+        return false;
+    });
+    }
+}
